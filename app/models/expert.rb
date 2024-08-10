d75548ede74b89328fa7e43398252518c51535ed
tuxtux59 <thibaud.renaux@gmail.com>
2024-08-10 18:28:50 +0200

    add min length on user and expert username cols

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,6 +1,7 @@
 class Expert < ApplicationRecord
   has_many :tasting_notes
 
-  validates_presence_of :username
   validates_presence_of :email
+
+  validates :username, presence: true, length: {minimum: 5}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :saved_searches
 
-  validates_presence_of :username
   validates_presence_of :email
+
+  validates :username, presence: true, length: {minimum: 5}
 end
