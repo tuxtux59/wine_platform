b0abb9d314cf20ad857056df2e8dcf5fe814f780
tuxtux59 <thibaud.renaux@gmail.com>
2024-08-11 00:07:34 +0200

    add tastingnote support on API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    resources :tasting_notes
     resources :bottles
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
