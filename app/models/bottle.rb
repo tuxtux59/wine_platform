18a0ee73d20e61e87bad5ba8d2b9b67bb55f6c16
tuxtux59 <thibaud.renaux@gmail.com>
2024-08-10 12:16:15 +0200

    create basic models

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,2 +1,13 @@
 class Bottle < ApplicationRecord
+  has_many :tasting_notes
+  has_many :price_histories
+
+  def average_score
+    tasting_notes.average(:score)
+  end
+
+  def latest_price
+    latest_history = price_histories.order(recorded_at: :desc).first
+    latest_history.price if latest_history
+  end
 end
