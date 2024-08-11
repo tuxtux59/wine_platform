51ef53c1d37ae7ccfbebdbf3880d2f9d9039361c
tuxtux59 <thibaud.renaux@gmail.com>
2024-08-11 17:46:28 +0200

    trigger notification on bottle creation

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -1,5 +1,7 @@
 # Class that represent a user search for wine bottles
 class SavedSearch < ApplicationRecord
+  scope :for_grape_variety, ->(grape_variety) { where("search_params->>'grape_variety' = ?", grape_variety) }
+
   belongs_to :user
 
   validates_presence_of :user
